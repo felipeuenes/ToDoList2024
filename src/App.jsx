e9564c8f47ef1ed70e0fc24fc175a0df6949fa8b
Felipe Uenes <uenesdesigner@gmail.com>
2024-03-14 18:28:33 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import { Screen } from './components/screen'
+
+export function App() {
+  
+
+  return (
+    <>
+    <div className='backgroundFull'>
+      
+      <Screen/>
+    </div>
+    </>
+  )
+}
+
+
---
+++ src/components/screen/index.jsx
@@ -0,0 +1,14 @@
+import "./style.css"
+
+
+
+export function Screen(){
+
+    return(
+       
+            <div className="fullScreen">
+                <h1>ToDoList</h1>
+            </div>
+       
+    )
+}
