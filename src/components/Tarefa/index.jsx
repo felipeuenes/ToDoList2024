a12fa69fd9abe8ed8eb196c3f1195d61a46d80da
Felipe Uenes <uenesdesigner@gmail.com>
2024-03-14 18:35:04 -0300

    add tarefa

---
+++ src/components/Tarefa/index.jsx
@@ -0,0 +1,11 @@
+import "./style.css"
+
+
+export function Tarefa(){
+
+    return(
+        <div className="tarefa">
+
+        </div>
+    )
+}
--- src/components/screen/index.jsx
+++ src/components/screen/index.jsx
@@ -7,7 +7,14 @@ export function Screen(){
     return(
        
             <div className="fullScreen">
+                <section>
                 <h1>ToDoList</h1>
+
+                </section>
+                <section>
+                    <input type="text" placeholder="Pesquise..." />
+                </section>
+
             </div>
        
     )
